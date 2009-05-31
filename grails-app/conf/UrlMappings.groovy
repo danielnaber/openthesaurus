b4e4c9a60ddfe5f822d527286e304165b10607c0
dnaber <naber@danielnaber.de>
2009-05-31 12:54:54 +0000

    offer nice 404 page

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,15 +1,22 @@
 class UrlMappings {
-    static mappings = {
+
+  static mappings = {
+      "404"(view:"/errors/notFound")
+      
       "/$controller/$action?/$id?" {
         constraints {
             // apply constraints here
         }
       }
+      
       // the search result page:
       "/overview"(controller:'redirect', action:'overview')
+      
       // the synset page:
       // name clash, must be handled in SynsetController
+ 
       // word detail page:
       "/word_detail"(controller:'redirect', action:'worddetail')
-    }
+  }
+
 }
