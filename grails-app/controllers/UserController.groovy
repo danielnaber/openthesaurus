5881eff46d47eb6bd13a1e80c17c1ed868036718
dnaber <naber@danielnaber.de>
2009-08-08 22:04:05 +0000

    adding a salt to each encrypted password

--- grails-app/controllers/UserController.groovy
+++ grails-app/controllers/UserController.groovy
@@ -289,6 +289,9 @@ class UserController extends BaseController {
     }
 
     public static String md5sum(String str) {
+        // a pseudo-random salt:
+        final String salt = "hi234z2ejgrr97otw4ujzt4wt7jtsr4975FERedefef"
+        str = str + "/" + salt
         MessageDigest md = MessageDigest.getInstance("MD5")
         md.update(str.getBytes(), 0, str.length())
         byte[] md5sum = md.digest()
