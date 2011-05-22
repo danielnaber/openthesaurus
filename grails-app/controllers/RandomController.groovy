d7b2385c4a2b862f096316353c79a96817b550bb
dnaber <naber@danielnaber.de>
2011-05-22 20:51:51 +0000

    add a page with random synsets

---
+++ grails-app/controllers/RandomController.groovy
@@ -0,0 +1,55 @@
+/**
+ * vithesaurus - web-based thesaurus management tool
+ * Copyright (C) 2011 Daniel Naber, www.danielnaber.de
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */ 
+
+import java.sql.Connection
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+import com.vionto.vithesaurus.Synset
+
+class RandomController {
+    
+    def dataSource
+    
+    def synsets = {
+        Connection conn
+        PreparedStatement ps
+        ResultSet resultSet
+        List synsets = []
+        try {
+          conn = dataSource.getConnection()
+          String sql = "SELECT id FROM synset WHERE is_visible = 1 ORDER BY RAND() LIMIT 10"
+          ps = conn.prepareStatement(sql)
+          resultSet = ps.executeQuery()
+          while (resultSet.next()) {
+              synsets.add(Synset.get(resultSet.getInt("id")))
+          }
+        } finally {
+          if (resultSet != null) {
+            resultSet.close()
+          }
+          if (ps != null) {
+            ps.close()
+          }
+          if (conn != null) {
+            conn.close()
+          }
+        }
+        [synsets: synsets]
+    }
+    
+}
---
+++ grails-app/views/random/synsets.gsp
@@ -0,0 +1,33 @@
+<%@page import="com.vionto.vithesaurus.*" %>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="layout" content="main" />
+        <title><g:message code="random.title" /></title>
+        <meta name="robots" content="noindex, nofollow"/>        
+    </head>
+    <body>
+
+        <hr/>
+
+        <p><g:link controller="woerter"><g:message code="word.list.backlink" /></g:link></p>
+
+        <h2><g:message code="random.headline" /></h2>
+
+        <g:if test="${flash.message}">
+            <div class="message">${flash.message}</div>
+        </g:if>
+
+        <div class="list">
+            <ul>
+                <g:each in="${synsets}" status="i" var="synset">
+                    <li><g:link controller="synset" action="edit" id="${synset.id}">${synset.toShortStringWithShortLevel(Integer.MAX_VALUE, true).encodeAsHTML()}</g:link></li>
+                </g:each>
+            </ul>
+        </div>
+        
+        <br/><br/>
+        <g:link action="synsets" params="${[time:System.currentTimeMillis()]}"><g:message code="random.reload" /></g:link>
+
+    </body>
+</html>
