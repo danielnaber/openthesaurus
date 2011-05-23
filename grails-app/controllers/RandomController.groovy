52920c97d073fa41feed3b40c72ba150e8073723
dnaber <naber@danielnaber.de>
2011-05-23 18:12:42 +0000

    make this controller extend BaseController

--- grails-app/controllers/RandomController.groovy
+++ grails-app/controllers/RandomController.groovy
@@ -21,7 +21,7 @@ import java.sql.PreparedStatement
 import java.sql.ResultSet
 import com.vionto.vithesaurus.Synset
 
-class RandomController {
+class RandomController extends BaseController {
     
     def dataSource
     
