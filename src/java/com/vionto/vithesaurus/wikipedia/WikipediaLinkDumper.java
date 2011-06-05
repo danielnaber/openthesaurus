7006d7ecfa910f1f24b0ab717c074874b7460c84
dnaber <naber@danielnaber.de>
2011-06-05 09:11:27 +0000

    move classes to sub package

--- src/java/com/vionto/vithesaurus/wikipedia/WikipediaLinkDumper.java
+++ src/java/com/vionto/vithesaurus/wikipedia/WikipediaLinkDumper.java
@@ -14,8 +14,8 @@
  *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */ 
-package com.vionto.vithesaurus;
+ */
+package com.vionto.vithesaurus.wikipedia;
 
 import java.io.FileInputStream;
 import java.io.IOException;
