7509238f86e8ce10c94f018143f7d044632bc6ff
dnaber <naber@danielnaber.de>
2012-04-01 12:11:29 +0000

    allow ^2 and ^3 in terms

--- grails-app/domain/com/vionto/vithesaurus/Term.groovy
+++ grails-app/domain/com/vionto/vithesaurus/Term.groovy
@@ -38,7 +38,7 @@ class Term implements Comparable, Cloneable {
     /** Allowed term expression. TODO: read from configuration */
     final static String TERM_REGEXP =
         "[ 0-9a-zA-ZöäüÖÄÜßëçèéáàóòÈÉÁÀÓÒãñíîş\\{\\}\\\"\\?\\*=()\\-\\+/.,'_:<>;%°" +
-            "\\!\\[\\]&#αΑβΒγΓδΔεΕζΖηΗθΘιΙκΚλΛμΜνΝξΞοΟπΠρΡσΣτΤυΥφΦχΧψΨωΩœ]+"
+            "\\!\\[\\]²³&#αΑβΒγΓδΔεΕζΖηΗθΘιΙκΚλΛμΜνΝξΞοΟπΠρΡσΣτΤυΥφΦχΧψΨωΩœ]+"
 
     static belongsTo = [synset:Synset]
     
