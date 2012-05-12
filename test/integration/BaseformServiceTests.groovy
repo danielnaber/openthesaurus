61c6a4570af182cc42160031db631bae3a36efe3
dnaber <naber@danielnaber.de>
2012-05-12 08:43:42 +0000

    add test for baseform service

---
+++ test/integration/BaseformServiceTests.groovy
@@ -0,0 +1,64 @@
+/**
+ * vithesaurus - web-based thesaurus management tool
+ * Copyright (C) 2012 Daniel Naber (www.danielnaber.de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import grails.test.GrailsUnitTestCase
+
+import java.sql.Connection
+
+class BaseformServiceTests extends GrailsUnitTestCase {
+
+    def dataSource
+    def baseformService
+
+    void testBaseformService() {
+        Connection conn = dataSource.getConnection()
+        try {
+            initDatabase(conn)
+            Set forms
+
+            forms = baseformService.getBaseForms(conn, "unbekannteswort")
+            assertEquals(0, forms.size())
+
+            forms = baseformService.getBaseForms(conn, "Haus")
+            assertEquals(0, forms.size())
+
+            forms = baseformService.getBaseForms(conn, "Hauses")
+            assertEquals("[Haus]", forms.toString())
+
+            // compounds:
+            forms = baseformService.getBaseForms(conn, "Testhäuser")
+            assertEquals("[Testhaus]", forms.toString())
+
+        } finally {
+            conn.close()
+        }
+    }
+
+    private void initDatabase(Connection conn) {
+        exec("DROP TABLE IF EXISTS word_mapping", conn)
+        exec("CREATE TABLE IF NOT EXISTS `word_mapping` (fullform varchar(255) CHARACTER SET utf8 NOT NULL, " +
+                "baseform varchar(255) CHARACTER SET utf8 NOT NULL, KEY idx (fullform)) ENGINE=MyISAM DEFAULT CHARSET=latin1", conn)
+        exec("INSERT INTO word_mapping (baseform, fullform) VALUES ('Haus', 'Hauses')", conn)
+        exec("INSERT INTO word_mapping (baseform, fullform) VALUES ('Haus', 'Häuser')", conn)
+    }
+
+    private void exec(String sql, Connection conn) {
+        def statement = conn.prepareStatement(sql)
+        statement.execute()
+    }
+}
