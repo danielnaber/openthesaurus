e65143b49e3d7dc3b67dc8b5fb16c5d8f04114e3
Daniel Naber <naber@danielnaber.de>
2013-02-23 16:19:15 +0100

    don't allow all characters for the callback name

--- grails-app/controllers/SynsetController.groovy
+++ grails-app/controllers/SynsetController.groovy
@@ -257,6 +257,10 @@ class SynsetController extends BaseController {
     private void renderApiResult(SearchResult searchResult, ArrayList similarTerms, List partialMatchResult, List startsWithResult) {
         if (params.format == "application/json") {
             if (params.callback) {
+                String validCallbackPattern = '^[a-zA-Z0-9_-]+$'
+                if (!params.callback.matches(validCallbackPattern)) {
+                    throw new Exception("Invalid callback parameter: only characters [a-zA-Z0-9_-] are allowed")
+                }
                 // JSONP: Get the actual JSON content via HTTP and add the callback - all other ways to make
                 // this work failed or were equally ugly:
                 def paramsList = []
