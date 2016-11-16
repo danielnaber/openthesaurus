386ade9118bd92e079deb68c00cd236a7c3e0c14
Daniel Naber <naber@danielnaber.de>
2016-11-16 22:10:11 +0100

    don't send email if there are only few matches

--- mail_errors_hourly.sh
+++ mail_errors_hourly.sh
@@ -3,9 +3,13 @@
 
 FIRSTPART=naber
 LASTPART=danielnaber.de
+LIMIT=10
 
 tail -n 100000 tomcat2/logs/catalina.out | grep "`date +"%Y-%m-%d %H:"`" tomcat2/logs/catalina.out | egrep "ERROR|SEVERE" >/tmp/hourly_error_data.txt
 MATCHES=`cat /tmp/hourly_error_data.txt | wc -l`
 
-cat /tmp/hourly_error_data.txt | tail -n 100 | \
-        ifne mail -a 'Content-Type: text/plain; charset=utf-8' -s "Hourly OpenThesaurus Error Report ($MATCHES total)" ${FIRSTPART}@${LASTPART}
+if [ "$MATCHES" -gt "$LIMIT" ]
+then
+  cat /tmp/hourly_error_data.txt | tail -n 100 | \
+      ifne mail -a 'Content-Type: text/plain; charset=utf-8' -s "Hourly OpenThesaurus Error Report ($MATCHES total)" ${FIRSTPART}@${LASTPART}
+fi
