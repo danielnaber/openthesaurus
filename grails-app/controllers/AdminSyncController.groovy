fcfa9ebf2c0f7ca67486dab97f9f82c8b888250a
Daniel Naber <naber@danielnaber.de>
2018-09-12 17:16:25 +0200

    merge from master

--- grails-app/controllers/AdminSyncController.groovy
+++ grails-app/controllers/AdminSyncController.groovy
@@ -28,36 +28,34 @@ class AdminSyncController extends BaseController {
     def beforeInterceptor = [action: this.&adminAuth]
     
     def index = {
-        File f = new File("/tmp/formulierung-mit-stil.de/tabkeywords.txt")
+        //File f = new File("/home/dnaber/thesaurus/abgleich/otus/otus_a_small.txt")
+        //File f = new File("/home/dnaber/thesaurus/abgleich/otus/otus_a.txt")
+        //File f = new File("/home/dnaber/thesaurus/abgleich/otus/otus_complete.txt")
+        File f = new File("/home/dnaber/thesaurus/abgleich/otus/otus_complete.txt")
         int headWordUnknown = 0  // a headword from the dictionary is not known at all
         int synWordUnknown = 0   // a synonym from the dictionary is not known at all
         int synUnknown = 0       // an entry has a synonym that's not known as a synonym in OT
         Scanner sc = new Scanner(f)
-        boolean stop = false
         int lineCount = 0
+        int displayCount = 0
         int from = params.from ? Integer.parseInt(params.from) : 0
         int pageSize = 100
+        render "<ul>"
         while (sc.hasNextLine()) {
             String line = sc.nextLine()
             if (line.startsWith("#")) {
                 continue
             }
-            //String[] parts = line.split("=>")
-            String[] parts = line.split(": ")
+            String[] parts = line.split("=>")
+            //String[] parts = line.split(": ")
             if (parts.length > 2) {
                 parts[1] = parts[1..parts.length-1].join(": ")
                 print "joined: " + parts[0] + " -> " + parts[1]
             } else if (parts.length != 2) {
-                render "skipping: " + line + "<br>"
+                //render "skipping: " + line + "<br>"
                 continue
             }
             lineCount++
-            if (lineCount < from) {
-                continue
-            }
-            if (lineCount > from + pageSize) {
-                continue
-            }
             String headWord = parts[0].trim()
             String[] synonyms = parts[1].trim().split(",\\s*|\\d\\.")
             headWord = headWord.replaceFirst("(.+), sich", "(sich) \$1")
@@ -71,65 +69,60 @@ class AdminSyncController extends BaseController {
                 headWordUnknown++
                 headWordFound = false
             }
+            if (displayCount > from + pageSize) {
+                print "limit reached at " + displayCount
+                break
+            }
+            int synMatches = 0
             for (String syn : synonyms) {
                 if (syn.trim().isEmpty()) {
                     continue
                 }
+                syn = syn.trim().replaceAll("\u00AD", "")
                 syn = syn.trim().replaceAll("\u202F", "")
-                String origSyn = syn
                 syn = syn.replace("sich ", "(sich) ")
+                syn = syn.replace(", sich", "(sich) ")
                 syn = syn.replace("jmd. ", "")
                 syn = syn.replace("etw. ", "")
                 syn = syn.replaceAll("^ein ", "")
                 syn = syn.replaceAll("^eine ", "")
                 syn = syn.trim()
                 List otResult = getMatches(syn)
-                if (otResult.size() == 0) {
-                    //render "Synonym word unknown: '" + syn + "' (" + line + ")<br>"
-                    //render "Synonym word unknown: " + syn + " - " + parts[1] + "<br>"
-                    String encQuery = URLEncoder.encode(syn, "utf-8")
-                    render lineCount + ". <a target='ot' href='https://www.openthesaurus.de/synonyme/" + URLEncoder.encode(parts[0], "utf-8") + "'>" + parts[0] + "</a>" +
-                            " - <a target='ot' href='https://www.openthesaurus.de/synonyme/" + 
-                            encQuery + "'>" + origSyn + "</a>" +
-                            " (<a href='http://www.google.de/search?hl=de&source=hp&biw=&bih=&q=%22" + encQuery + "%22&btnG=Google-Suche&gbv=1'>G</a>)<br>"
-                    /*for (def c in syn) {
-                        render "*" + ((int)c)
+                if (otResult.size() > 0) {
+                    synMatches++
+                } else {
+                    print "lineCount " + lineCount + ", displayCount " + displayCount
+                    if (displayCount < from) {
+                        displayCount++
+                        continue
                     }
-                    render "<br>"
-                    */
-                    synWordUnknown++
-                    if (synWordUnknown > 100) {
-                        stop = true
-                    }
-                } else if (headWordFound) {  // if the headword isn't even found we don't need to count more detailed
-                    //TODO: buggy, need to work on synsets
-                    /*boolean synFound = false
-                    for (Term t : otResult) {
-                        if (syn == t.word) {
-                            render "X: " + syn + " == " + t.word + "<br>"
-                            synFound = true
-                            break
-                        }
-                    }
-                    render "debug: " + headWord + " ~ " + syn + ": " + headWordFound + " - " + synFound + "<br>"
-                    render "debug: " + otResult + "<br>"
-                    if (!(headWordFound && synFound)) {
-                        render "Synonym relation unknown: '" + headWord + "' ~ '" + syn + "'<br>"
-                        synUnknown++
-                    }*/
+                    String enc = URLEncoder.encode(syn, "utf-8")
+                    String style = " style='color:lightblue' "
+                    render "<li>" + displayCount + ". <a target='ot' href='https://www.openthesaurus.de/synonyme/" + URLEncoder.encode(syn, "utf-8") + "'>" +
+                            syn + "</a> <a $style target='_blank' href='https://de.wiktionary.org/wiki/" + enc +"'>Wiktionary</a> " +
+                            //"<a target='_blank' href='https://www.startpage.com/do/asearch?q=" + enc + "'>Google</a> " +
+                            "<a $style target='_blank' href='http://www.google.de/search?hl=de&source=hp&biw=&bih=&q=\"" + enc + "\"'>Google</a> " +
+                            line + "<br>\n"
+                    displayCount++
                 }
             }
-            if (stop) {
-                render "STOPPING"
-                break
-            }             
+            /*if (!headWordFound && synMatches == 0) {
+                render displayCount + ". <a target='ot' href='https://www.openthesaurus.de/synonyme/" + URLEncoder.encode(parts[0].trim(), "utf-8") + "'>" +
+                        parts[0].trim() + "</a>"
+                for (String syn : synonyms) {
+                    if (syn.trim().isEmpty()) {
+                        continue
+                    }
+                    String encQuery = URLEncoder.encode(syn, "utf-8")
+                    render " - <a target='ot' href='https://www.openthesaurus.de/synonyme/" + encQuery + "'>" + syn + "</a>"
+                }
+                render " - <br>"
+                displayCount++
+            }*/
         }
+        render "</ul>"
         render "---<br>"
-        render "DONE:<br>"
-        render "headWordUnknown: " + headWordUnknown + "<br>"
-        render "synWordUnknown: " + synWordUnknown + "<br>"
         render "<a href='?from=" + (from+pageSize) + "'>next</a>"
-        //render "synUnknown: " + synUnknown + "<br>"
     }
 
     private List getMatches(String str) {
