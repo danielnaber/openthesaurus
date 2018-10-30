389dc92ad0ea4e84a9c47c0c9793f016b4419e0e
Kenny Stuart <465920+kstuart@users.noreply.github.com>
2018-10-30 17:00:47 +0000

    enable setting locale from application property (thesaurus.serverId)

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,8 @@
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+
 // Place your Spring DSL code here
 beans = {
+    localeResolver(SessionLocaleResolver) {
+        defaultLocale= new java.util.Locale(grailsApplication.config.thesaurus.serverId);
+    }
 }
