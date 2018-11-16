7fdcc06a3890c62569241e1a597c0d7312739d54
Daniel Naber <naber@danielnaber.de>
2018-11-16 14:57:54 +0100

    fix path

--- re-deploy.sh
+++ re-deploy.sh
@@ -2,7 +2,7 @@
 #dnaber, 2013-09-11
 #helper script to deploy new version by overwriting the old one
 
-rm target/openthesaurus-*.war
+rm ./build/libs/openthesaurus-*.war
 grails war && \
   scp -i ~/.ssh/ot2017 ./build/libs/openthesaurus-*.war root@83.169.2.105:/home/openthesaurus/openthesaurus.war && \
   ssh -i ~/.ssh/ot2017 root@83.169.2.105 "unzip -d /home/openthesaurus/tomcat/webapps/ROOT/ /home/openthesaurus/openthesaurus.war && /etc/init.d/tomcat8 restart"
