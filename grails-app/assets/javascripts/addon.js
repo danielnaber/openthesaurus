796b5e50f9d1bb841da90cb8d1618d07d4fddcc5
Christopher Blum <christopher.blum@spellboy.com>
2020-01-27 14:13:24 +0100

    improvements for langaugetool layer

--- grails-app/assets/javascripts/addon.js
+++ grails-app/assets/javascripts/addon.js
@@ -2,9 +2,13 @@ $(function() {
     if (!location.search.match(/lt-test/)) {
         return;
     }
+
     function openLayer(url) {
+        $("html").addClass("disable-scrolling");
+
         var shadow = $("<div />", { "class": "addon-layer__shadow" }).click(function() {
             shadow.remove();
+            $("html").removeClass("disable-scrolling");
         }).appendTo("body");
         var layer = $("<div />", { "class": "addon-layer" }).appendTo(shadow).click(function(e) {
             e.stopPropagation();
@@ -12,12 +16,13 @@ $(function() {
         var header = $("<div />", { "class": "addon-layer__header" }).appendTo(layer);
         var close = $("<div />", { "class": "addon-layer__close" }).appendTo(layer).click(function() {
             shadow.remove();
+            $("html").removeClass("disable-scrolling");
         });
         var headline1 = $("<div />", { "class": "addon-layer__headline1" }).appendTo(header);
         var logo1 = $("<div />", { "class": "addon-layer__logo-ot" }).appendTo(headline1);
         var seperator = $("<div />", { "class": "addon-layer__logo-seperator", text: "+" }).appendTo(headline1);
-        var logo2 = $("<div />", { "class": "addon-layer__logo-lt" }).appendTo(headline1);
-        var video = $("<video />", { "class": "addon-layer__video", muted: "muted", loop: true, autoplay: true, width: 480, src: 'https://languagetoolplus.com/videos/addon.mp4' }).appendTo(header);
+        var logo2 = $("<a />", { "class": "addon-layer__logo-lt", href: "https://languagetool.org/", target: "_blank", title: "LanguageTool - Grammatik- und Rechtschreibprüfung" }).appendTo(headline1);
+        var video = $("<video />", { "class": "addon-layer__video", muted: "muted", loop: true, autoplay: true, width: 420, src: 'https://languagetoolplus.com/videos/addon.mp4' }).appendTo(header);
         var headline2 = $("<div />", { "class": "addon-layer__headline2", text: "Professioneller schreiben mit OpenThesaurus und LanguageTool" }).appendTo(layer);
         var text = $("<div />", { "class": "addon-layer__text", html: "Mit diesem kostenlosen Plugin können Sie nicht nur überall im Web (z.&nbsp;B. in Gmail, WordPress oder Google Docs) Synonyme abrufen, sondern bekommen auch intelligente Vorschläge zur Verbesserung der <strong>Grammatik und Zeichensetzung</strong> Ihrer Texte." }).appendTo(layer);
         var button = $("<a />", { "class": "addon-layer__button", text: "Kostenlos installieren", href: url, target: "_blank" }).appendTo(layer).click(function() {
